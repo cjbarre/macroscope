ee096b868474c24020b4271e1359542c3aeac0ab
Cameron Barre <cjbarre@gmail.com>
2019-10-23 18:29:56 -0700

    Add data sources.
    Add support for the household portion of the employment situation report and the JOLT survey.

--- bin/init.sh
+++ bin/init.sh
@@ -10,6 +10,10 @@ echo "Ingesting Establishment Employment Situation"
 
 ./bin/bls-loader.sh establishment_employment_situation
 
-# ./bin/bls-loader.sh household_employment_situation
+echo "Ingesting Household Employment Situation"
 
-# ./bin/bls-loader.sh job_openings_and_labor_turnover_survey
+./bin/bls-loader.sh household_employment_situation
+
+echo "Ingesting Job Opening and Labor Turnover Survey (JOLTS)"
+
+./bin/bls-loader.sh jolts
