70dbd4875d4c2820535f12d416e92592551d7860
Cameron Barre <cjbarre@gmail.com>
2019-10-23 20:58:43 -0700

    Create anonymous role automatically for postgrest.
    Grant select to warehouse after each data import.

--- bin/init.sh
+++ bin/init.sh
@@ -6,14 +6,22 @@ echo "Waiting for storage to initialize."
 
 sleep 5
 
+psql -h $DB_HOST -U $DB_USER -d $DB_NAME -f storage/create-anon-role.sql
+
 echo "Ingesting Establishment Employment Situation"
 
 ./bin/bls-loader.sh ees
+
+psql -h $DB_HOST -U $DB_USER -d $DB_NAME -f storage/grant-select.sql
 
 echo "Ingesting Household Employment Situation"
 
 ./bin/bls-loader.sh hes
 
+psql -h $DB_HOST -U $DB_USER -d $DB_NAME -f storage/grant-select.sql
+
 echo "Ingesting Job Opening and Labor Turnover Survey (JOLTS)"
 
 ./bin/bls-loader.sh jolts
+
+psql -h $DB_HOST -U $DB_USER -d $DB_NAME -f storage/grant-select.sql
---
+++ storage/grant-select.sql
@@ -0,0 +1,3 @@
+grant usage on schema warehouse to macroscope_anon;
+
+grant select on all tables in schema warehouse to macroscope_anon;
