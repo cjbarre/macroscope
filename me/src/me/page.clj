491d2a240afa6076d171e2f374f81859602915da
Cameron Barre <cjbarre@gmail.com>
2019-12-01 19:14:07 -0800

    Adjust layout.

--- me/src/me/page.clj
+++ me/src/me/page.clj
@@ -1,8 +1,7 @@
 (ns me.page)
 
-(defn main-chart [series-title]
-  {:height 500
-   :width 1200
+(defn main-chart [data series-title]
+  {:data data
    :mark {:type "area"}
    :title {:text series-title
            :fontSize 20}
@@ -12,17 +11,37 @@
                   :scale {:domain {:selection "brush"}}}
               :y {:field "value" :type "quantitative"}}})
 
-(def details-chart 
-  {:height 100
-   :width 1200
+(defn details-chart [data]
+  {:data data
    :mark "area"
    :selection {:brush {:type "interval" :encodings ["x"]}}
    :encoding {:x {:field "period" :type "temporal" :timeUnit "utcyearmonth"}
               :y {:field "value" :type "quantitative"}}})
 
+(defn rate-of-change-12-mo [data]
+  {:data data
+   :mark "area"
+   :encoding {:x {:field "period" :type "temporal" :timeUnit "utcyearmonth"}
+              :y {:field "value" :type "quantitative"}}})
+
 (defn page [series-id series-title]
-  [:div {:style "margin:0 auto;width:1200;"}
-   [:vega-lite {:data {:url (format "data/%s.json" series-id)}
-                :vconcat [(main-chart series-title)
-                          details-chart]}]])
+  (let [main-data {:url (format "data/%s.json" series-id)
+                   :name "data"
+                   :format {:type "json"
+                            :property "primary_data"}}
+        roc-12-mo-data (assoc-in main-data [:format :property] "rate_of_change_12_mo")]
+    [:div.page-container.grid
+     [:div.page-header
+      [:div.series-title series-title]]
+     [:section.page-body
+      [:div.data-section
+       [:div.data-section-header 
+        "Data Values (10 Years)"]
+       [:div.data-section-content 
+        [:img.chart {:src "/assets/test.svg"}]]]
+      [:div.data-section
+       [:div.data-section-header
+        "Rate of Change YTD (12 Month Lookback)"]
+       [:div.data-section-content
+        [:img.chart {:src "/assets/test2.svg"}]]]]]))
 
