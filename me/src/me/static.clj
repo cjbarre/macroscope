08ea1fb82c0bc7b1f20da99ff497c3ee2de8dba8
Cameron Barre <cjbarre@gmail.com>
2019-12-01 19:13:37 -0800

    Add raw assets to live build target.

--- me/src/me/static.clj
+++ me/src/me/static.clj
@@ -149,7 +149,10 @@
   (oz/build!
    [{:from "build/raw/"
      :to "build/site/"
-     :template-fn site-template}])
+     :template-fn #'site-template}
+    {:from "build/raw/assets/"
+     :to "build/site/assets/"
+     :as-assets? true}])
   
   
 
