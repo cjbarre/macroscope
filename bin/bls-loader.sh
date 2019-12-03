b63c38cfa298cad7b69de7df7ea26405f1944aaa
Cameron Barre <cjbarre@gmail.com>
2019-12-03 11:17:38 -0800

    Undo

--- bin/bls-loader.sh
+++ bin/bls-loader.sh
@@ -4,7 +4,3 @@ cat configuration/$1/tables.csv |
 parallel -C , "wget {2} -q -O - |
                tsv2csv |
                pgfutter --schema warehouse --table $1_{1} csv"
-
-
-ls | head -n 10 |
-parallel "vl2vg {1} | vg2svg | sed 's/width=\"[0-9]*\" height=\"[0-9]*\"//' > svg/{1}.svg"
