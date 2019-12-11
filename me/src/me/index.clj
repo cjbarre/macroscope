aa1c32899f1cf132a4ebe4ad8ec0827a0a968437
Cameron Barre <cjbarre@gmail.com>
2019-12-11 14:25:16 -0800

    Responsive styling and copy work.

--- me/src/me/index.clj
+++ me/src/me/index.clj
@@ -2,22 +2,26 @@
   (:require [me.meca :as meca]))
 
 (def page 
-  [:div#body-container
+  [:div#body-container.tes
    [:div.landing-page-article
     [:div.landing-page-article-title :meca/title]
     [:div.landing-page-article-meta
      [:img.landing-page-article-author-img {:src "/assets/cam.png"}]
      [:div.landing-page-article-author :meca/author]]
     [:div.landing-page-article-content
+
      [:p.landing-page-article-greeting :meca/greeting]
      [:p :meca/favor]
      [:p :meca/body-p-1]
      [:p :meca/body-p-2]
      [:p :meca/body-p-3]
-     [:a.landing-page-product-1 
-      {:href "/reference/v1/bls/ces/"
-       :target "_blank"} 
-      :meca/product-1]]]])
+     [:p [:a.landing-page-product-1
+          {:href "/reference/v1/bls/ces/"
+           :target "_blank"}
+          :meca/product-1]]
+     [:p :meca/product-1-desc]
+     [:p :meca/product-1-desc-2]
+     [:p :meca/outro]]]])
 
 (spit "build/raw/index.clj"
       (format "(ns raw.index) \n \n %s"
--- me/src/me/template.clj
+++ me/src/me/template.clj
@@ -8,13 +8,13 @@
   [:div#main-container
    vega-embed-js
    [:link {:rel "stylesheet" :type "text/css" :href "/assets/app.css"}]
-   [:div#header-container
-    [:div.site-title "Mortal Economics"]
-    [:div.site-title "|"]
-    [:div.site-title [:a {:href "#"} "FAQ"]]
-    [:div.site-title "|"]
-    [:div.site-title [:a {:href "#"} "Terms"]]
-    [:div.site-title "|"]
-    [:div.site-title
+   [:ul#header-container
+    [:li.site-title "Mortal Economics"]
+    #_[:div.site-title "|"]
+    [:li.site-title [:a {:href "#"} "FAQ"]]
+    #_[:div.site-title "|"]
+    [:li.site-title [:a {:href "#"} "Terms"]]
+    #_[:div.site-title "|"]
+    [:li.site-title
      [:a {:href "/reference/v1/bls/ces/" :target "_blank"} "Open The Reference"]]]
    content])
