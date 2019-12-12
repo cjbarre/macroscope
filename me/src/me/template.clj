030b41261dbe2ade3f6d3d922d27c853bb8026ff
Cameron Barre <cjbarre@gmail.com>
2019-12-12 09:25:07 -0800

    Adjust styling, add Blog link to nav.

--- me/src/me/template.clj
+++ me/src/me/template.clj
@@ -10,11 +10,9 @@
    [:link {:rel "stylesheet" :type "text/css" :href "/assets/app.css"}]
    [:ul#header-container
     [:li.site-title "Mortal Economics"]
-    #_[:div.site-title "|"]
+    [:li.site-title [:a {:href "#"} "ToS"]]
     [:li.site-title [:a {:href "#"} "FAQ"]]
-    #_[:div.site-title "|"]
-    [:li.site-title [:a {:href "#"} "Terms"]]
-    #_[:div.site-title "|"]
+    [:li.site-title [:a {:href "#"} "Blog"]]
     [:li.site-title
      [:a {:href "/reference/v1/bls/ces/" :target "_blank"} "Open The Reference"]]]
    content])
