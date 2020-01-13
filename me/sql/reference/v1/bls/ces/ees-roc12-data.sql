e2c2b8d64d102b73b17f197b547255212fc4f388
Cameron Barre <cjbarre@gmail.com>
2020-01-13 11:47:11 -0800

    Handle edge case for beginning of year.

--- me/sql/reference/v1/bls/ces/ees-roc12-data.sql
+++ me/sql/reference/v1/bls/ces/ees-roc12-data.sql
@@ -3,16 +3,15 @@ with x as (
         year,
         period,
         value::float                  as current,
-        lag(value::float, 12) over () as previous
+        lag(value::float, 12) over (order by series_id, year, period) as previous
     from warehouse.ees_data
-    where year::float between date_part('year', now()) - 1 and date_part('year', now())
+    where year::int between 2018 and 2019
     and period <> 'M13'
-    order by year, period
 )
 select series_id,
     json_agg(json_build_object('value', trunc(cast((current / previous - 1) * 100 as numeric), 3),
                                 'period', year || '-' || regexp_replace(period, 'M', '') || '-01T23:00:00Z')
                 order by x.year, x.period) as data
 from x
-where year = date_part('year', now())::text
+where year = '2019'
 group by x.series_id;
